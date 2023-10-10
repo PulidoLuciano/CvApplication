c86e2584ba881c8cc4bd580de35f07c2b02c19cb
Luciano Pulido <lucianonicolaspulido@gmail.com>
2023-10-10 20:27:59 -0300

    Handle add studies and jobs and set state Created event handlers for adding jobs and studies and created jobs and studies states to save and display them later

--- CV-Application/src/components/generator.jsx
+++ CV-Application/src/components/generator.jsx
@@ -6,12 +6,52 @@ function Generator(){
     const [addingJob, setAddingJob] = useState(false);
     const [addingStudy, setAddingStudy] = useState(false);
     const [personalData, setPersonalData] = useState({name:"", email:"", phone:"", location:""});
+    const [newStudy, setNewStudy] = useState({school:"", degree:"", startDate:"", endDate:"", location:""});
+    const [newJob, setNewJob] = useState({company:"", position:"", startDate:"", endDate:"", location:"", description:""});
+    const [jobs, setJobs] = useState([]);
+    const [studies, setStudies] = useState([]);
     
     function handlePersonalChange(event){
         let field = event.target.name;
         let newPersonal = {...personalData};
         newPersonal[field] = event.target.value;
         setPersonalData(newPersonal);
+    }
+
+    function handleStudyChange(event){
+        let field = event.target.name;
+        let study = {...newStudy};
+        study[field] = event.target.value;
+        setNewStudy(study);
+    }
+
+    function handleJobChange(event){
+        let field = event.target.name;
+        let job = {...newJob};
+        job[field] = event.target.value;
+        setNewJob(job);
+    }
+
+    function handleStudySubmit(event){
+        event.preventDefault();
+        let study = {school:"", degree:"", startDate:"", endDate:"", location:""};
+        for(let i = 0; i < 5; i++){
+            study[event.target[i].name] = event.target[i].value;
+        }
+        let newStudies = studies.concat(study);
+        setStudies(newStudies);
+        event.target.reset();
+    }
+
+    function handleJobSubmit(event){
+        event.preventDefault();
+        let job = {company:"", position:"", startDate:"", endDate:"", location:"", description:""};
+        for(let i = 0; i < 6; i++){
+            study[event.target[i].name] = event.target[i].value;
+        }
+        let newJobs = jobs.concat(job);
+        setJobs(newJobs);
+        event.target.reset();
     }
 
     return (
@@ -33,17 +73,17 @@ function Generator(){
                 !addingStudy 
                 ? <button onClick={() => setAddingStudy(true)}>Add new study</button>
                 : <>
-                    <form onSubmit={() => {preventDefault()}}>
+                    <form onSubmit={handleStudySubmit}>
                         <label htmlFor="">School</label>
-                        <input type="text" name="school"/>
+                        <input type="text" name="school" onChange={handleStudyChange}/>
                         <label htmlFor="">Degree</label>
-                        <input type="text" name="degree"/>
+                        <input type="text" name="degree" onChange={handleStudyChange}/>
                         <label htmlFor="">Start Date</label>
-                        <input type="text" name="startDate"/>
+                        <input type="date" name="startDate" onChange={handleStudyChange}/>
                         <label htmlFor="">End Date</label>
-                        <input type="text" name="endDate"/>
+                        <input type="date" name="endDate" onChange={handleStudyChange}/>
                         <label htmlFor="">Location</label>
-                        <input type="text" name="location"/>
+                        <input type="text" name="location" onChange={handleStudyChange}/>
                         <button>Add</button>
                         <button onClick={() => setAddingStudy(false)}>Cancel</button>
                     </form>
@@ -57,19 +97,19 @@ function Generator(){
                 !addingJob 
                 ? <button onClick={() => setAddingJob(true)}>Add new experience</button>
                 : <>
-                    <form onSubmit={() => {preventDefault(); }}>
+                    <form onSubmit={handleJobSubmit}>
                         <label htmlFor="">Company Name</label>
-                        <input type="text" name="company"/>
+                        <input type="text" name="company" onChange={handleJobChange}/>
                         <label htmlFor="">Position</label>
-                        <input type="text" name="position"/>
+                        <input type="text" name="position" onChange={handleJobChange}/>
                         <label htmlFor="">Start Date</label>
-                        <input type="text" name="startDate"/>
+                        <input type="date" name="startDate" onChange={handleJobChange}/>
                         <label htmlFor="">End Date</label>
-                        <input type="text" name="endDate"/>
+                        <input type="date" name="endDate" onChange={handleJobChange}/>
                         <label htmlFor="">Location</label>
-                        <input type="text" name="location"/>
+                        <input type="text" name="location" onChange={handleJobChange}/>
                         <label htmlFor="">Description</label>
-                        <textarea name="description" id="" cols="40" rows="7" maxLength="250"></textarea>
+                        <textarea name="description" id="" cols="40" rows="7" maxLength="250" onChange={handleJobChange}></textarea>
                         <button>Add</button>
                         <button onClick={() => setAddingJob(false)}>Cancel</button>
                     </form>
